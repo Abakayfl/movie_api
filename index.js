b74e8ce1f07eed6e04dedc8c03c3db394b4cd2c4
Abakayfl <73713860+Abakayfl@users.noreply.github.com>
2021-02-08 23:27:09 -0500

    enable cors for local react app/all domains

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const Directors = Models.Director;
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(morgan('common'));
